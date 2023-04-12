c53c11744f726f7a30fb79a54fce2bb482046c57
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2023-04-12 05:57:55 +0200

    5push

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ import genfig as fig
 import utils
 import scipy.sparse as sc
 # path = os.path.abspath(os.getcwd())
-# cl = np.loadtxt(path + "/1-cl.txt", dtype=float)
-# dom = np.loadtxt(path + "/2-dom.txt", dtype=float)
-# num = np.loadtxt(path + "/2-num.txt", dtype=int)
-cl, dom, num = utils.loadfile(4, cl=False)
+cl, dom, num = utils.loadfile(3, cl=False)
 cl = utils.findCL(dom, 2)
+"""
 contour=np.loadtxt("4-contourObj.txt")
 two=np.empty(contour.shape[0])
 two.fill(2)
 # to fix ?
 u= sc.csc_matrix((two, (contour[:,0],contour[:,1])), shape=(cl.shape[0], cl.shape[1])).toarray()
 cl=cl+u
+"""
 #np.savetxt("2-cl.txt", cl)
 #=np.load("psi2.npy")
 #cl=np.loadtxt("2-cl.txt")
